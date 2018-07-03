c62fc10822d49338ebaa2e9aaf0d95f989fa4545
Christian Thomas <christian.thomas.2894@gmail.com>
2018-07-03 19:23:03 +0000

    automatically backed up by learn

---
+++ lib/genre.rb
@@ -0,0 +1,20 @@
+
+class genre
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+
+end
